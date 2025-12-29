c7eb8d47985f8ad772890bfd477e43a2818415c2
tunadeveloper <tunabusiness25@gmail.com>
2025-12-29 15:12:14 +0300

    Updated

---
+++ Core/ZenBlog.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace ZenBlog.Application.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> _validators) : IPipelineBehavior<TRequest, TResponse> where TRequest : class
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validationResult = await Task.WhenAll(
+                _validators.Select(x => x.ValidateAsync(context)));
+
+            var failures = validationResult.Where(x => x is not null)
+                .SelectMany(x => x.Errors).ToList();
+
+            if (failures.Any())
+            {
+                var errorDetails = failures.GroupBy(x => x.PropertyName)
+                    .ToDictionary(x => x.Key, x => x.Select(x => x.ErrorMessage)
+                    .ToArray())
+                    .ToList();
+
+                throw new ValidationException(failures);
+            }
+        }
+        return await next();
+    }
+}
